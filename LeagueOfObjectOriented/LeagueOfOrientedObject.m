df3b646f7653b69196dcd2ee739b65d183d9a7f1
michelzf <31206123@095819-CRT.local>
2013-12-12 15:36:04 -0200

    rgerthehteht

--- LeagueOfObjectOriented/LeagueOfOrientedObject.m
+++ LeagueOfObjectOriented/LeagueOfOrientedObject.m
@@ -21,22 +21,21 @@
 
 -(void)jogar {
     
-    //int aux;
+    int aux;
     //Arma *esp = [Arco new];
-    Arma *esp = [[Arco alloc] initWith:100];
-    Jogador *j1 = [[Jogador alloc] initWithNome:@"Joaquim" raca:2];
-    Jogador *j2 = [[Jogador alloc] initWithNome:@"Pedro" raca:2];
+    
+    Jogador *j1 = [[Jogador alloc] initWithNome:@"Joaquim" raca:0];
+    Jogador *j2 = [[Jogador alloc] initWithNome:@"Pedro" raca:0];
     srand((unsigned int)time(NULL));
     //j1.nome=@"Joaquim";
-    [j1 setArmaPrimaria:esp];
     //j2.nome=@"Pedro";
-    /*NSLog(@"escolha a classe do jogador 1:");
-     scanf("%d",&aux);
-     j1.raca = aux;
-     NSLog(@"escolha a classe do jogador 2:");
-     scanf("%d",&aux);
-     j2.raca = aux;*/
-    NSLog(@"Jogador 1 classe %d , jogador 2 classe %d",j1.raca,j2.raca);
+    NSLog(@"escolha a classe do jogador 1:");
+    scanf("%d",&aux);
+    j1.raca = aux;
+    NSLog(@"escolha a raça do jogador 2:");
+    scanf("%d",&aux);
+    j2.raca = aux;
+    NSLog(@"Jogador 1 raça %d , jogador 2 raça %d",j1.raca,j2.raca);
     [j1 ataque: j2];
     NSLog(@"vida = %.2f ataqueJ = %d defesa= %d", j2.vida, j1.ataque,j2.defesa);
     [j1 ataque: j2];
